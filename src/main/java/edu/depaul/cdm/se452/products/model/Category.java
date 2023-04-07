6c341c6349ca76cb803ba0ee0426a31e8cfa4071
joshua hershberger <jhershbe@depaul.edu>
2023-04-07 01:07:02 -0400

    updating generated value type

--- src/main/java/edu/depaul/cdm/se452/products/model/Category.java
+++ src/main/java/edu/depaul/cdm/se452/products/model/Category.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Table
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
--- src/main/java/edu/depaul/cdm/se452/products/model/Product.java
+++ src/main/java/edu/depaul/cdm/se452/products/model/Product.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Table
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int categoryId;
     private String manufacturer;
