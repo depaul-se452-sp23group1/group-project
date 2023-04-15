5847d2f0efc31f6cafd6a855f6e4bfb9417ce147
bramhashreerm <braghava@depaul.edu>
2023-04-15 01:32:39 -0500

    Updated Transactions and Shipping classes

---
+++ src/main/java/edu/depaul/cdm/se452/transaction/model/Shipping.java
@@ -0,0 +1,20 @@
+package edu.depaul.cdm.se452.transaction.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Shipping {
+
+    private String shpId;
+    private String shpMethod;
+    @Column(name="address_1")
+    private String address1;
+    @Column(name="address_2")
+    private String address2;
+    private String city;
+    private String zip;
+    private String state;
+    @JoinColumn(name="tr_id", referencedColumnName = "transaction_id")
+    private Transactions transaction;
+}
