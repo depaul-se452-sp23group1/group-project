16ea3ae3768b5c5b808a01aa7eb0b9a913838eb6
bramhashreerm <braghava@depaul.edu>
2023-04-16 03:13:42 -0500

    Updated Shipping class

--- src/main/java/edu/depaul/cdm/se452/transaction/model/Shipping.java
+++ src/main/java/edu/depaul/cdm/se452/transaction/model/Shipping.java
@@ -1,4 +1,29 @@
 package edu.depaul.cdm.se452.transaction.model;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "shipping")
 public class Shipping {
+
+    @Id
+    @Column(name="shp_id")
+    private long shpId;
+    @Column(name="shp_method")
+    private String shpMethod;
+    @Column(name="shp_status")
+    private String shpStatus;
+    @Column(name="address_1")
+    private String address1;
+    @Column(name="address_2")
+    private String address2;
+    private String city;
+    private String zip;
+    private String state;
+    // @JoinColumn(name="tr_id", referencedColumnName = "tr_id")
+    //  private Transactions transaction;
+    @Column(name="tr_id")
+    private long trId;
 }
+
--- src/main/java/edu/depaul/cdm/se452/transaction/repository/ShippingRepository.java
+++ src/main/java/edu/depaul/cdm/se452/transaction/repository/ShippingRepository.java
@@ -1,5 +1,15 @@
 package edu.depaul.cdm.se452.transaction.repository;
+import edu.depaul.cdm.se452.transaction.model.Shipping;
+import edu.depaul.cdm.se452.transaction.model.Transactions;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ShippingRepository{
+import java.util.List;
 
+
+public interface ShippingRepository extends CrudRepository<Shipping, Long> {
+
+    Shipping findById(long id);
+
+    List<Shipping> findAll();
 }
+
