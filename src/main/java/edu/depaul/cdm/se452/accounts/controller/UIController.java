2145f49b0ee7f0f536ad013619e8b6edd2c59f8e
karthikChandrasekaran10 <kchandr3@depaul.edu>
2023-06-04 23:54:22 -0500

    UI added

---
+++ src/main/java/edu/depaul/cdm/se452/accounts/controller/UIController.java
@@ -0,0 +1,12 @@
+package edu.depaul.cdm.se452.accounts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UIController {
+    @RequestMapping(value = "/customer/login")
+    public String Customer(){
+        return "Login";
+    }
+}
